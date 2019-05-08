0cc0f097507b25d0949dc8c9756f1e348977be11
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-05-09 10:51:21 +1200

    Add carousel placeholder.

--- client/components/List.jsx
+++ client/components/List.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Carousel from './Carousel'
+import ListItem from './ListItem'
 
 const List = () => {
     return (
-        <div>
-            <p>Articles go here.</p>
+        <div className='content'>
+            <Carousel />
+            <p>
+                <ListItem />
+                {/*       {props.articleDB.map(article => {
+        return (
+          <ListItem
+          key={article.id}
+          article={article}
+          />
+        )
+      })} */}
+            </p>
         </div>
     )
 }
