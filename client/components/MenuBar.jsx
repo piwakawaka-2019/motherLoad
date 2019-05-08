54fa49cc5c9f5cd84817da3256cb785bdbd6470c
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-05-09 11:58:46 +1200

    Link Menubar to routes.

--- client/components/MenuBar.jsx
+++ client/components/MenuBar.jsx
@@ -4,10 +4,10 @@ const MenuBar = () => {
     return (
         <div className='menubar'>
             <div className='columns'>
-            <label className="column is-3 label is-medium has-text-centered"> <a href='#'>PC Parts</a></label>
-            <label className="column is-3 label is-medium has-text-centered"><a href='#'>Tutorials</a></label>
-            <label className="column is-3 label is-medium has-text-centered"><a href='#'>Tips</a></label>
-            <label className="column is-3 label is-medium has-text-centered"><a href='#'>Articles</a></label>          
+            <label className="column is-3 label is-medium has-text-centered"> <a href='/pcparts/'>PC Parts</a></label>
+            <label className="column is-3 label is-medium has-text-centered"><a href='/tutorials'>Tutorials</a></label>
+            <label className="column is-3 label is-medium has-text-centered"><a href='/tips'>Tips</a></label>
+            <label className="column is-3 label is-medium has-text-centered"><a href='/articles'>Articles</a></label>          
             </div>
         </div>
     )
