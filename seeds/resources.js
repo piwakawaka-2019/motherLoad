f1f1bad29dfbde9b85b132e75e6ce8e7a4af1f69
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-05-10 11:21:48 +1200

    updated db

--- seeds/resources.js
+++ seeds/resources.js
@@ -8,7 +8,6 @@ exports.seed = function (knex, Promise) {
           id: 1,
           title: 'Compromise',
           description: 'You do not need the most expensive pieces to start off start of with a good base and work your way up',
-          category: 'General Knowledge',
           type: 'tip',
           source_url: ""
         },
@@ -16,7 +15,6 @@ exports.seed = function (knex, Promise) {
           id: 2,
           title: 'Take What You Need Not What You Want.',
           description: 'Don’t get a very large power supply just because you can unless you’re going to use the capacity of it.',
-          category: 'Power Supply',
           type:'tip',
           source_url: ""
         },
