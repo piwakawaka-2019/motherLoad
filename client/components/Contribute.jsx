11e5e5bd28ef95574cd92cf52348dbee0e0edf05
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-05-14 14:00:43 +1200

    Minor CSS adjust

--- client/components/Contribute.jsx
+++ client/components/Contribute.jsx
@@ -4,7 +4,9 @@ import {connect} from 'react-redux'
 const Contribute = () => {
     return (
         <div>
-            <a href='#/addtip'><i className="fas fa-plus" />&nbsp;Add Tip</a>
+            <p>Know something we don't? &nbsp;<span className='advice'>
+                <a href="#/advice"><i className='fas fa-plus' />&nbsp;Add a Tip</a></span>
+            </p>
         </div>
     )
 }
