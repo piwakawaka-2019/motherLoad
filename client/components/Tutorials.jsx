2090fbf7522c3abaf5d26b5a17a6014351acdff9
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-05-15 22:49:48 +1200

    merge

--- client/components/Tutorials.jsx
+++ client/components/Tutorials.jsx
@@ -37,6 +37,7 @@ class Tutorials extends React.Component {
             return (
               <Fragment>
                 <div id="adviceBox">
+                <h3 className="has-text-white">{tutorials.title}</h3>
                 <embed width="420" height="315" src={tutorials.source_url} />
                   {this.props.auth.isAuthenticated && (
                     <Buttons id={tutorials.id} />
